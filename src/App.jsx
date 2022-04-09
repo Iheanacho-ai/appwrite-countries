7faf97a742dfefaaf09ce6e1e73a86f6c8ed6829
Amarachi Iheanacho <amarachi2812@gmail.com>
2022-04-09 23:00:32 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import {Appwrite} from "appwrite";
+import {useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [countries, setCountries] = useState(["country"])
+  const sdk = new Appwrite();
+
+  sdk
+    .setEndpoint('http://localhost/v1') // Your API Endpoint
+    .setProject('6251d12c44f38f85595c') // Your project ID
+  ;
+
+  
+  
+  const getCountries = async (e) => {
+    try {
+      let buttonId = e.target.id
+      sdk.setLocale(buttonId)
+      let promise = await sdk.locale.getCountries()
+      setCountries([])
+      promise.countries.map((country)=> setCountries(prevArray => [...prevArray, country.name]))
+      
+    } catch (error) {
+      console.log(error)
+      
+    }
+
+  }
+
+
+  
+
+  return (
+    <div className="App">
+      <div className="app-container">
+        <div className="button-container">
+          <button id='en' onClick={getCountries}>English</button>
+          <button id='ar' onClick={getCountries}>Arabic</button>
+          <button id='zh-cn' onClick={getCountries}>Chinese - China</button>
+          <button id='sl' onClick={getCountries}>Slovenian</button>
+          <button id='tr' onClick={getCountries}>Turkish</button>
+
+        </div>
+        <select name="coutnries" id="countries-list">
+          {
+            countries.map((country) => (
+              <option value="country">{country}</option>
+            ))
+          }
+        </select>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
